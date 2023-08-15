d86330c0cee706ebbafad34e93ba2d1d168db844
najadams <najmadams1706@gmail.com>
2023-08-15 15:56:57 -0700

    second commit of 4-new_in_list.py

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-def new_in_list(my_list, idx, element):
-    if my_list  and idx < len(my_list) and idx >= 0:
-        new_list = my_list.copy()
-        new_list[idx] = element
-        return new_list
-
+#def new_in_list(my_list, idx, element):
+    if idx < 0 or idx >= len(my_list):
+        return my_list.copy()
+    new_list = my_list.copy()
+    new_list[idx] = element
+    return new_list
