bfdc76a4c262429c3148329ac95d531a80704b2a
najadams <najmadams1706@gmail.com>
2023-08-15 13:57:58 -0700

    tried fixing the second file

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    if idx >= 0 or idx < len(my_list):
-        for item in range(len(my_list) - 1, -1, -1):
-            print("{:d}".format.(my_list[item]))
+    if idx < 0 or idx >= len(my_list):
+        return my_list
+
+    new_list = my_list.copy()
+    new_list[idx] = element
+    return new_list
