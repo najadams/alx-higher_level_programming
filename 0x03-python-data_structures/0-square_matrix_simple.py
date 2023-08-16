e97c4c50181c15ff274aaa34f01aa58a0c856d68
najadams <najmadams1706@gmail.com>
2023-08-16 14:23:26 -0700

    testing for uniq_add

--- 0x03-python-data_structures/0-square_matrix_simple.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-def square_matrix_simple(matrix=[]):
-    result = matrix.copy()
-    for i in range(len(result)):
-        result[i] = list(map(lambda x:x**2,result[i]))
-    return (result)
---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    uniq = list(filter(lambda x: my_list.count() == 1, my_list))
+    total = sum(uniq)
+    return (total)
