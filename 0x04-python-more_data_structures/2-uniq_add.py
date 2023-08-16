22cba38060e3e6e65d07dc92ad040e19cd015f2e
najadams <najmadams1706@gmail.com>
2023-08-16 15:33:35 -0700

    completed task two

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
     uniq = list(filter(lambda x: my_list.count(x) == 1, my_list))
+    for x in uniq:
+        if x not in my_lis  and isinstance(x,(int,float)):
+            uniq.append(x)
     total = sum(uniq)
     return (total)
