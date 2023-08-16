f4aa7d73d08e7f04c76c979cc31e10c0ab246696
najadams <najmadams1706@gmail.com>
2023-08-16 15:39:17 -0700

    fixed the function

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
     uniq = list(filter(lambda x: my_list.count(x) == 1, my_list))
-    for x in uniq:
-        if x not in my_lis  and isinstance(x,(int,float)):
+    for x in my_list:
+        if x not in uniq  and isinstance(x,(int,float)):
             uniq.append(x)
     total = sum(uniq)
     return (total)
