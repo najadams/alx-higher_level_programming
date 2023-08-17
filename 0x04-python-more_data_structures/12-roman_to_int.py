c5045f2b5ef8ffe8aaa38b7791f6d731ab609fb7
najadams <najmadams1706@gmail.com>
2023-08-17 05:24:28 -0700

    fixed white space

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,19 +1,13 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     rom_n = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    num = last_rom = 0
-    list_num = [0]
-    for ch in roman_string:
-        if ch in rom_n:
-            if rom_n[ch] <= last_rom:
-                max_list = max(list_num)
-                num += max_list - sum(n for n in list_num if max_list > n)
-                list_num = [rom_n[ch]]
-            else:
-                list_num.append(rom_n[ch])
-            
-            last_rom = rom_n[ch]
-
-    max_list = max(list_num)
-    num += max_list - sum(n for n in list_num if max_list > n)
+    num = 0
+    prev_value = 0
+    for ch in reversed(roman_string):
+        value = rom_n.get(ch, 0)
+        if value >= prev_value:
+            num += value
+        else:
+            num -= value
+        prev_value = value
     return num
