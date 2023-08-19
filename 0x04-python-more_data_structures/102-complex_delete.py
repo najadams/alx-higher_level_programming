7ca5461f38711f0f90ae6b21336e3fc28158a32b
najadams <najmadams1706@gmail.com>
2023-08-18 18:42:39 -0700

    added the necessary method to the dictionary

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    for key, value_dic in a_dictionary:
+    for key, value_dic in a_dictionary.items():
         if value_dic == value:
             del a_dictionary[key]
     return (a_dictionary)
